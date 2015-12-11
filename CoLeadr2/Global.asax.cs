b2e40e7a9f7a445d74b168b26ac975dc9e484327
tbrenneison <tori.brenneison@gmail.com>
2015-12-10 19:37:39 -0500

    backup commit

--- CoLeadr2/Global.asax.cs
+++ CoLeadr2/Global.asax.cs
@@ -14,7 +14,7 @@ namespace CoLeadr2
     {
         protected void Application_Start()
         {
-           // Database.SetInitializer(new DropCreateDatabaseAlways<CoLeadr2Context>());
+            //Database.SetInitializer(new DropCreateDatabaseAlways<CoLeadr2Context>());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
